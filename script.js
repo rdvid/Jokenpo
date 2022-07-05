0a1ba6cfa6da95faac7b6587373754943dc978e3
Rafael D <rafael_dvid@hotmail.com>
2022-07-05 16:25:40 -0300

    refactoring functions, adding dynamic score

--- script.js
+++ script.js
@@ -1,71 +1,98 @@
 const jokenpo = ["Rock", "Paper", "Scissors"];
 var playerScore = 0;
 var computerScore = 0;
+let playerSelection;
+let messageScreen = document.getElementById('messageScreen');
+let buttonSelected;
 
-// a function who returns randomly one of jokenpo options
+// change top screen message
+function changeMessage(message){
+    messageScreen.innerHTML = message;
+}
+
+
+// returns randomly one of jokenpo options
 function computerPlay(){
     return jokenpo[Math.floor((Math.random() * jokenpo.length))]
 }
 
-// a function for string treatment to prevent errors
-function caseFix(string){
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
+
+// buttons settings
+document.getElementById('rockButton').addEventListener('click', function(){
+    buttonSelected = jokenpo[0];
+    oneRound(buttonSelected);
+});
+
+document.getElementById('paperButton').addEventListener('click', function(){
+    buttonSelected = jokenpo[1];
+    oneRound(buttonSelected);
+});
+    
+document.getElementById('scissorsButton').addEventListener('click', function(){
+    buttonSelected = jokenpo[2];
+    oneRound(buttonSelected);
+});
+
+document.getElementById('playerWins').innerHTML = playerScore;
+
+document.getElementById('cpuWins').innerHTML = computerScore;
+
+
 
 // one round of jokenpo
-function oneRound(playerSelection, computerSelection){
-    playerSelection = caseFix(playerSelection)
-    computerSelection = caseFix(computerSelection)
+function oneRound(buttonSelected){
+
+    playerSelection = buttonSelected;
+    let computerSelection = computerPlay();
+
     if(playerSelection === computerSelection){
-        return "oh-ho, it's a Draw!"
+        return changeMessage("oh-ho, it's a Draw!");
     }else{
         if(playerSelection === "Rock"){
             switch(computerSelection){
                 case "Scissors":
                     playerScore++
-                    return "You Win! Rock > Scissors!"
+                    return changeMessage("You Win! Rock > Scissors!");
                 case "Paper":
                     computerScore++
-                    return "You Lose! Paper > Rock!"
+                    return changeMessage("You Lose! Paper > Rock!");
             }
         }else if(playerSelection === "Paper"){
             switch(computerSelection){
                 case "Rock":
                     playerScore++
-                    return "You Win! Paper > Rock!"
+                    return changeMessage("You Win! Paper > Rock!");
                 case "Scissors":
                     computerScore++
-                    return "You Lose! Scissors > Paper!"
+                    return changeMessage("You Lose! Scissors > Paper!");
             }
         }else{
             switch(computerSelection){
                 case "Rock":
                     computerScore++
-                    return "You Lose! Rock > Scissors"
+                    return changeMessage("You Lose! Rock > Scissors");
                 case "Paper":
                     playerScore++
-                    return "You Win! Scissors > Paper"
-                    
+                    return changeMessage("You Win! Scissors > Paper");
             }
         }
     }
 }
 
 
-// 5 rounds of jokenpo
+//5 rounds of jokenpo
 function game(){
     for(let cont = 0; cont < 5; cont++){
-        let playerSelection = prompt("Rock, Paper or Scissors?");
-        let computerSelection = computerPlay();
-        console.log(oneRound(playerSelection, computerSelection))
-        console.log(`Player: ${playerScore} X Machine: ${computerScore}`)
+        oneRound();
+        
     }
+
     if(playerScore === computerScore){
-        console.log("Draw!! Restart page to play again!")
+        changeMessage("Draw!! Restart page to play again!");
     }else if(playerScore > computerScore){
-        console.log("You're the Winner!! Press F5 to play again :)")
+        changeMessage("You're the Winner!! Press F5 to play again :");
     }else{
-        console.log("You Lose! :c Press F5 for an rematch")
+        changeMessage("You Lose! :c Press F5 for an rematch");
     }
 }
 
